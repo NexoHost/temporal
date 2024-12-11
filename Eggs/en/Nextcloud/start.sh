f29621ffd948ff943ea90d5da4499f11232ddaff
NexoHost Corporation <96266038+NexoHost@users.noreply.github.com>
2024-12-11 23:07:33 +0100

    Update start.sh

--- Eggs/en/Nextcloud/start.sh
+++ Eggs/en/Nextcloud/start.sh
@@ -8,10 +8,3 @@ echo "🛠 Starting Nginx..."
 echo "✅ Successful startup of the EGG by NexoHost.cloud"
 
 # Arte ASCII para mostrar al final del inicio exitoso
-
-echo " _   _                _   _           _         _                 _ "
-echo "| \ | |              | | | |         | |       | |               | |"
-echo "|  \| | _____  _____ | |_| | ___  ___| |_   ___| | ___  _   _  __| |"
-echo "| . ` |/ _ \ \/ / _ \|  _  |/ _ \/ __| __| / __| |/ _ \| | | |/ _` |"
-echo "| |\  |  __/>  < (_) | | | | (_) \__ \ |_ | (__| | (_) | |_| | (_| |"
-echo "\_| \_/\___/_/\_\___/\_| |_/\___/|___/\__(_)___|_|\___/ \__,_|\__,_|"
